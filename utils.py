b0e49f95e33f87765a28bd7c08f5b34454f657f9
YJZ182123 <51749038+YJZ182123@users.noreply.github.com>
2023-04-03 11:45:19 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,10 +11,10 @@ google_drive_paths = {
 
     "Disney.pt" : "https://drive.google.com/uc?id=1z51gxECweWXqSYQxZJaHOJ4TtjUDGLxA",
     "Disney_FreezeSG.pt" : "https://drive.google.com/uc?id=1PJaNozfJYyQ1ChfZiU2RwJqGlOurgKl7",
-    "Disney_StructureLoss.pt" : "https://drive.google.com/uc?id=1PILW-H4Q0W8S22TO4auln1Wgz8cyroH6",
+    "Disney_LS.pt" : "https://drive.google.com/uc?id=1PILW-H4Q0W8S22TO4auln1Wgz8cyroH6",
     
     "Metface_FreezeSG.pt" : "https://drive.google.com/uc?id=1P5T6DL3Cl8T74HqYE0rCBQxcq15cipuw",
-    "Metface_StructureLoss.pt" : "https://drive.google.com/uc?id=1P65UldIHd2QfBu88dYdo1SbGjcDaq1YL",
+    "Metface_LS.pt" : "https://drive.google.com/uc?id=1P65UldIHd2QfBu88dYdo1SbGjcDaq1YL",
 }
 
 def download_pretrained_model(download_all=True, file=''):
